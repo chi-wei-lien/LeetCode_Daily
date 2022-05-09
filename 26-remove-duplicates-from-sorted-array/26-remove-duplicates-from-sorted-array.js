c0e036d1dbd12cd2b10a56a149a8942782c50a6e
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-05-09 19:39:30 -0400

    Time: 185 ms (15.59%), Space: 44.7 MB (67.64%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let prev = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === prev) {
+            nums.splice(i, 1);
+            i--;
+        }
+        prev = nums[i];
+    }
+};
