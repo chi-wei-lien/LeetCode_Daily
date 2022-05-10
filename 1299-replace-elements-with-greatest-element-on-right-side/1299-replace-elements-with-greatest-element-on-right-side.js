bd8ecc0aa0051a4f186e2c59dd7f1370fae8acdb
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-05-10 10:26:55 -0400

    Time: 129 ms (51.67%), Space: 45 MB (90.81%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -3,16 +3,11 @@
  * @return {number[]}
  */
 var replaceElements = function(arr) {
-    let n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        let big = arr[i + 1];
-        for (let j = i + 2; j < n; j++) {
-            if (arr[j] > big) {
-                big = arr[j];
-            }
-        }
-        arr[i] = big;
+    let max = -1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let cur = arr[i];
+        arr[i] = max;
+        if (cur > max) max = cur;
     }
-    arr[n - 1] = -1;
     return arr;
 };
