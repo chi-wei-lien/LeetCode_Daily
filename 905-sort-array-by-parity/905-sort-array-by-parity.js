50ef5036ec934f156d57214c3d5c4e9e548bc6e1
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-05-10 20:54:29 -0400

    Time: 84 ms (84.66%), Space: 43.8 MB (99.26%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    let result = [];
+    let index = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] % 2 == 0) {
-            result.unshift(nums[i]);
-        }
-        else {
-            result.push(nums[i]);
+            let temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+            index++;
         }
     }
-    return result;
+    return nums;
 };
