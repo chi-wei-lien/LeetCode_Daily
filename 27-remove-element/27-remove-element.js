59c4aeb2e0d7a6634c47789addb6dfc123da61a7
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-05-10 20:59:10 -0400

    Time: 93 ms (28.31%), Space: 41.9 MB (82.78%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -4,10 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    let index = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == val) {
-            nums.splice(i, 1);
-            i--;
+        if (nums[i] != val) {
+            nums[index] = nums[i];
+            index++;
         }
     }
+    return index;
 };
