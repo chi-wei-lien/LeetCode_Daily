119f746798e201046fca385495564e81adc9892e
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-05-14 09:26:11 -0400

    Time: 81 ms, Memory: 42.1 MB - LeetHub

---
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    return recursiveFunc(root);
+};
+
+var recursiveFunc = function(root, array = []) {
+    if (!root) return [];
+    recursiveFunc(root.left, array);
+    recursiveFunc(root.right, array);
+    array.push(root.val);
+    return array;
+}
