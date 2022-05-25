02cf184699f1158ed3bce1ddd37cd57ad958c569
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-05-24 22:34:27 -0400

    Time: 238 ms (94.32%), Space: 72.9 MB (12.54%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let stack = [[temperatures[0], 0]];
+    let distance = 0;
+    for (let i = 1; i < temperatures.length; i++) {
+        for (let j = stack.length - 1; j >= 0; j--) {
+            if (stack[stack.length - 1][0] < temperatures[i]) {
+                temperatures[stack[stack.length - 1][1]] = i - stack[stack.length - 1][1];
+                stack.pop();
+                distance--;
+                continue;
+            }
+            break;
+        }
+        stack.push([temperatures[i], i]);
+    }
+    for (let i = 0; i < stack.length; i++) {
+        temperatures[stack[i][1]] = 0;
+    }
+    return temperatures;
+};
