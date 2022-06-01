099b5cda10f8fd10fec26c16dfce073c87961be8
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-06-01 16:50:10 -0400

    Time: 123 ms (21.71%), Space: 44.9 MB (17.40%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -6,29 +6,25 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, newColor) {
-    let queue = [[sr, sc]];
+    let stack = [[sr, sc]];
     let originalColor = image[sr][sc];
     if (originalColor === newColor) {
         return image;
     }
-    while (queue.length) {
-        let queueLength = queue.length;
-        for (let i = 0; i < queueLength; i++) {
-            let curr = queue.shift();
-            if (curr[0] >= image.length || curr[1] >= image[0].length || curr[0] < 0 || curr[1] < 0) {
-                continue;
-            }
-            let currColor = image[curr[0]][curr[1]];
-            if (currColor != originalColor) {
-                continue;
-            }
-            console.log(curr);
-            image[curr[0]][curr[1]] = newColor;
-            queue.push([curr[0] + 1, curr[1]]);
-            queue.push([curr[0], curr[1] + 1]);
-            queue.push([curr[0] - 1, curr[1]]);
-            queue.push([curr[0], curr[1] - 1]);
+    while (stack.length) {
+        let curr = stack.pop();
+        if (curr[0] >= image.length || curr[1] >= image[0].length || curr[0] < 0 || curr[1] < 0) {
+            continue;
         }
+        let currColor = image[curr[0]][curr[1]];
+        if (currColor != originalColor) {
+            continue;
+        }
+        image[curr[0]][curr[1]] = newColor;
+        stack.push([curr[0] + 1, curr[1]]);
+        stack.push([curr[0], curr[1] + 1]);
+        stack.push([curr[0] - 1, curr[1]]);
+        stack.push([curr[0], curr[1] - 1]);
     }
     return image;
 };
