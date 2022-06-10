874ebed695c5d1c73209d97d5d19c3b77c519c6b
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-06-10 07:46:04 -0400

    Time: 86 ms (74.86%), Space: 43.7 MB (19.41%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -6,18 +6,12 @@
 var twoSum = function(nums, target) {
     let map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i]) && nums[i] * 2 === target) {
-            return [map.get(nums[i]), i];
-        }
-        map.set(nums[i], i);
-    }
-    for (let i = 0; i < nums.length; i++) {
         if (map.has(target - nums[i])) {
             let complement = map.get(target - nums[i]);
-            if (complement === i) {
-                continue;
-            }
             return [i, complement];
+        }
+        else {
+            map.set(nums[i], i);
         }
     }
     return [];
