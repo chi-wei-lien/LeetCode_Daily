c5e3a1c1804c6c8f282fae21b1c8c6c99cf1a491
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-06-17 22:28:36 -0400

    Time: 107 ms (80.34%), Space: 46.2 MB (82.75%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if (s.length === 0) return 0;
+    let result = 1;
+    let lpoint = 0;
+    let map = new Map();
+    map.set(s[0], 0);
+    for (let i = 1; i < s.length; i++) {
+        let lvalue = s[lpoint];
+        let rvalue = s[i];
+        if (map.has(rvalue)) {
+            let last = map.get(rvalue) + 1;
+            if (last > lpoint) {
+                lpoint = last;
+            }
+        }
+        let curString = s.substring(lpoint, i + 1);
+        // console.log(curString);
+        let newLength = curString.length;
+        if (newLength > result) {
+            result = newLength;
+        }
+        // console.log(rvalue + " " + i + " left " + lpoint);
+        map.set(rvalue, i);
+    }
+    return result;
+};
