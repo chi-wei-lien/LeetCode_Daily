a51f07270c3bc37c9dbd659c223f9d91753f8be4
chi-wei-lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-06-20 22:07:41 -0400

    Time: 106 ms (57.64%), Space: 43.6 MB (64.03%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x === 1) return 1;
+    let lpoint = 0;
+    // let rpoint = Math.floor(x / 2);
+    let rpoint = x;
+    let last = 0;
+    while (lpoint <= rpoint) {
+        let mid = Math.floor((rpoint + lpoint) / 2);
+        let pow = mid * mid;
+        if (pow === x) {
+            return mid;
+        }
+        if (pow < x) {
+            last = mid;
+            lpoint = mid + 1;
+        }
+        else if (pow > x) {
+            rpoint = mid - 1;
+        }
+    }
+    return last;
+};
