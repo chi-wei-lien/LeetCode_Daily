f5c5c39b23502d17e399d96e1089d962b55dbca1
Chi-Wei Lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-08-15 15:36:10 -0400

    Time: 124 ms (32.45%), Space: 44.5 MB (66.51%) - LeetHub

---
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+  if (nums.length === 0) return null;
+  return recursive(nums, 0, nums.length - 1);
+};
+
+var recursive = function(nums, left, right) {
+  if (left > right) {
+    return null;
+  }
+  let mid = (left + right) / 2 | 0;
+  let node = new TreeNode(nums[mid]);
+  if (left !== mid) node.left = recursive(nums, left, mid - 1);
+  node.right = recursive(nums, mid + 1, right);
+  return node;
+}
