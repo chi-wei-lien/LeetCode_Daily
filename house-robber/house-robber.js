7ab200ced5e98c1daa3b6456f24ca629f6fb42d9
Chi-Wei Lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-08-16 17:19:34 -0400

    Time: 93 ms, Memory: 42.4 MB - LeetHub

---
+++ house-robber/house-robber.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length === 1) return nums[0];
+    if (nums[1] < nums[0]) nums[1] = nums[0];
+    for (let i = 2; i < nums.length; i++) {
+        let cur = nums[i];
+        if ((cur + nums[i - 2]) > nums[i - 1]) {
+            let temp = nums[i - 1];
+            nums[i] = cur + nums[i - 2];
+            nums[i - 1] = temp;
+        } else {
+            nums[i] = nums[i - 1];
+        }
+    }
+    return nums[nums.length - 1] > nums[nums.length - 2] ? nums[nums.length - 1] : nums[nums.length - 2];
+};
