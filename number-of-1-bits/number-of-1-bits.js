540cbb7eb7be73b3857bfc150480211f9c7baed6
Chi-Wei Lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-08-20 23:33:18 -0400

    Time: 66 ms, Memory: 42.5 MB - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    for (let i = 0; i < 32; i++) {
+        if (n >> i & 1) {
+            count++;
+        }
+    }
+    return count;
+};
