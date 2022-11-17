52b7926dc19b4957a5150920318cd0a8c5f3b47f
Chi-Wei Lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-11-16 21:05:10 -0500

    Time: 82 ms (85.96%), Space: 48.2 MB (93.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+      int max = nums[0];
+      for (int i = 0; i < nums.size(); i++)
+      {
+        if (nums[i] == 0 && i >= max)
+        {
+          if (max >= nums.size() - 1)
+          {
+            return true;
+          }
+          return false;
+        }
+        if (nums[i] + i > max)
+        {
+          max = nums[i] + i;
+        }
+      }
+      if (max >= nums.size() - 1)
+      {
+        return true;
+      }
+      return false;
+    }
+};
