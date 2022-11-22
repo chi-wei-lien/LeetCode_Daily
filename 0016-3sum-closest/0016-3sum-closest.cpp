25b887cdfae5daa82d000ec3a9e08f6fbcb96be0
Chi-Wei Lien <89671525+chi-wei-lien@users.noreply.github.com>
2022-11-22 13:58:27 -0500

    Time: 99 ms (91.55%), Space: 10.2 MB (96.94%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -8,45 +8,30 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
       sort(nums.begin(), nums.end()); // nlogn
-      int curr;
       int left_p;
       int right_p;
       int sum;
       int diff;
       int min_diff = INT_MAX;
       int min_diff_sum;
-      for (int i = 0; i < nums.size(); i++)
+      for (int i = 0; i < nums.size() - 2; i++)
       {
-        curr = nums[i];
-        // 2 sum
-        left_p = 0;
-        right_p = nums.size() - 1;
+        left_p = i + 1; right_p = nums.size() - 1;
         while (left_p < right_p)
         {
-          if (left_p == i) left_p++;
-          if (right_p == i) right_p--;
-          if (left_p == right_p) break;
-          sum = nums[left_p] + nums[right_p];
-          diff = target - sum - curr;
-          if (diff == 0)
+          sum = nums[left_p] + nums[right_p] + nums[i];
+          if (sum > target) right_p--;
+          else if (sum < target) left_p++;
+          else return sum;
+          diff = abs(target - sum);
+          if (diff < min_diff)
           {
-            return sum + curr;
+            min_diff = diff;
+            min_diff_sum = sum;
           }
-          else if (sum > target - curr)
-          {
-            right_p--;
-          }
-          else
-          {
-            left_p++;
-          }
-        }
-        if (abs(diff) < min_diff)
-        {
-          min_diff = abs(diff);
-          min_diff_sum = sum + curr;
         }
       }
       return min_diff_sum;
     }
 };
+
